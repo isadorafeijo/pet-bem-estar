b55f574c1453eb6a53d43b843044f5204ab5b359
Isadora Santos Feijo <isadora.feijo2006@gmail.com>
2026-08-31 19:56:50 -0300

    atualiza Principal.java para testar especie, porte e status do agendamento

--- src/main/java/br/edu/ifrs/petbemestar/Principal.java
+++ src/main/java/br/edu/ifrs/petbemestar/Principal.java
@@ -9,13 +9,13 @@ public class Principal {
     public static void main(String[] args) {
         Dono dono = new Dono(1L, "Isadora", "51999999999");
 
-        Pet pet = new Pet(1L, "Luluca", dono);
+        Pet pet = new Pet(1L, "Luluca", Especie.CACHORRO, Porte.GRANDE, dono);
         dono.adicionarPet(pet);
 
         Agenda agenda = new Agenda(
                 1L,
                 pet,
-                TipoProcedimento.BANHO,          
+                TipoProcedimento.BANHO_TOSA,          
                 LocalDateTime.of(2026, 9, 1, 14, 0),
                 StatusAtendimento.MARCADO       
         );
@@ -25,5 +25,11 @@ public class Principal {
         System.out.println("Pet: " + pet.getNome());
         System.out.println("Procedimento: " + agenda.getTipoProcedimento());
         System.out.println("Status: " + agenda.getStatus());
+        
+        Agenda agendaAntiga = new Agenda(2L, pet, TipoProcedimento.BANHO_TOSA,
+                LocalDateTime.of(2026, 8, 20, 10, 0), StatusAtendimento.REALIZADO);
+        pet.adicionarAgendamento(agendaAntiga);
+
+        System.out.println("\nUltimo realizado: " + pet.ultimoAgendamentoRealizado().getHorario());
     }
 }
