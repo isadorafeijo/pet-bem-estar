0a6cb94fe20489bbcabdb812f553246038b6f639
Isadora Santos Feijo <isadora.feijo2006@gmail.com>
2026-08-31 20:38:02 -0300

    persiste dono, pet e agenda usando EntityManager

--- src/main/java/br/edu/ifrs/petbemestar/Principal.java
+++ src/main/java/br/edu/ifrs/petbemestar/Principal.java
@@ -3,33 +3,56 @@ package br.edu.ifrs.petbemestar;
 import java.time.LocalDateTime;
 
 import br.edu.ifrs.petbemestar.dominio.*;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class Principal {
 	
-    public static void main(String[] args) {
-        Dono dono = new Dono(1L, "Isadora", "51999999999");
+	public static void main(String[] args) {
 
-        Pet pet = new Pet(1L, "Luluca", Especie.CACHORRO, Porte.GRANDE, dono);
-        dono.adicionarPet(pet);
+        Dono isadora = new Dono(null, "Isadora", "51999999999");
 
-        Agenda agenda = new Agenda(
-                1L,
-                pet,
-                TipoProcedimento.BANHO_TOSA,          
-                LocalDateTime.of(2026, 9, 1, 14, 0),
-                StatusAtendimento.MARCADO       
-        );
-        pet.adicionarAgendamento(agenda);
+        Pet luluca = new Pet(null, "Luluca", Especie.CACHORRO, Porte.GRANDE, isadora);
+        Pet rex = new Pet(null, "Rex", Especie.CACHORRO, Porte.MEDIO, isadora);
 
-        System.out.println("Dono: " + dono.getNome());
-        System.out.println("Pet: " + pet.getNome());
-        System.out.println("Procedimento: " + agenda.getTipoProcedimento());
-        System.out.println("Status: " + agenda.getStatus());
-        
-        Agenda agendaAntiga = new Agenda(2L, pet, TipoProcedimento.BANHO_TOSA,
-                LocalDateTime.of(2026, 8, 20, 10, 0), StatusAtendimento.REALIZADO);
-        pet.adicionarAgendamento(agendaAntiga);
+        isadora.adicionarPet(luluca);
+        isadora.adicionarPet(rex);
 
-        System.out.println("\nUltimo realizado: " + pet.ultimoAgendamentoRealizado().getHorario());
-    }
+        luluca.adicionarAgendamento(new Agenda(null, luluca, TipoProcedimento.BANHO_TOSA,
+                LocalDateTime.of(2026, 9, 1, 14, 0), StatusAtendimento.MARCADO));
+        luluca.adicionarAgendamento(new Agenda(null, luluca, TipoProcedimento.BANHO_TOSA,
+                LocalDateTime.of(2026, 8, 20, 10, 0), StatusAtendimento.REALIZADO));
+        rex.adicionarAgendamento(new Agenda(null, rex, TipoProcedimento.BANHO_TOSA,
+                LocalDateTime.of(2026, 9, 3, 9, 0), StatusAtendimento.MARCADO));
+
+        System.out.println("Dono: " + isadora);
+        System.out.println("Pets: " + isadora.getPets());
+        System.out.println();
+
+        for (Pet pet : isadora.getPets()) {
+            System.out.println(pet + " | agendamentos: " + pet.getAgendamentos());
+        }
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pet-bem-estar-pu");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(isadora);
+
+        for (Pet pet : isadora.getPets()) {
+            em.persist(pet);
+        }
+
+        for (Pet pet : isadora.getPets()) {
+            for (Agenda agenda : pet.getAgendamentos()) {
+                em.persist(agenda);
+            }
+        }
+
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+	}
 }
